248077f67e8d680a34fae963f5ccd030cc6200fe
johnnylee-55 <leejohnny5533@gmail.com>
2022-11-07 10:06:46 -0800

    added week 16 changes - [BUG] CreateOrderTest.java does not pass

--- src/main/java/com/promineotech/jeep/entity/Jeep.java
+++ src/main/java/com/promineotech/jeep/entity/Jeep.java
@@ -4,6 +4,7 @@
 package com.promineotech.jeep.entity;
 
 import java.math.BigDecimal;
+import java.util.Comparator;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -14,7 +15,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class Jeep {
+public class Jeep implements Comparable<Jeep>{
   private Long modelPK;
   private JeepModel modelId;
   private String trimLevel;
@@ -25,5 +26,16 @@ public class Jeep {
   @JsonIgnore
   public Long getModelPK() {
     return modelPK;
+  }
+
+  @Override
+  public int compareTo(Jeep that) {
+    //@formatter:off
+    return Comparator
+        .comparing(Jeep::getModelId)
+        .thenComparing(Jeep::getTrimLevel)
+        .thenComparing(Jeep::getNumDoors)
+        .compare(this, that);
+    //@formatter:on
   }
 }
